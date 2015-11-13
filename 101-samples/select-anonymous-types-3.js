8f27a9971566f7b3780cfd4642300f95d792db63
Tania Shatilova <tatashat@gmail.com>
2015-11-13 22:03:49 +0300

    + datacontext.js with helper functions

--- 101-samples/select-anonymous-types-3.js
+++ 101-samples/select-anonymous-types-3.js
@@ -1,5 +1,5 @@
 (function() {
-    var products = getProductList(); 
+    var products = getProductList(); // function getProductList is in the file datacontext.js
     
     var productInfos = function(p){
         return { 
@@ -16,39 +16,4 @@
     result.forEach(function(item) {
         console.log(item.productName + " is in the category " + item.category + " and costs " + item.unitPrice + " per unit.");
     });
-
-    function getProductList() {
-        return [
-            {
-                id: 1,
-                productName: "Chai",
-                category: "Beverages",
-                unitPrice: "18.0000"
-            },
-            {
-                id: 2,
-                productName: "Chang",
-                category: "Beverages",
-                unitPrice: "19.0000"
-            },
-            {
-                id: 3,
-                productName: "Aniseed Syrup",
-                category: "Condiments",
-                unitPrice: "10.0000"
-            },
-            {
-                id: 4,
-                productName: "Chef Anton's Cajun Seasoning",
-                category: "Condiments",
-                unitPrice: "22.0000"
-            },
-            {
-                id: 5,
-                productName: "Chef Anton's Gumbo Mix",
-                category: "Condiments",
-                unitPrice: "21.3500"
-            },
-        ]
-    }    
 })();
